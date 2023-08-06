f481986b00678427c88f136a666a7b4d818c4248
Meme <myom142@hotmail.com>
2023-08-06 11:27:08 +0300

    update

--- resources/views/Card.blade.php
+++ resources/views/Card.blade.php
@@ -1,1 +1,18 @@
-<?= $card; ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="\app.css">
+    <title>2th blog</title>
+</head>
+<body>
+    <article>
+    <?=$card;?>
+    </article>
+    <a href="/">Go back</a>
+    
+</body>
+</html>
